6bba82476da1b11704ce4df50aec510ebd9e425a
ssousuke <wesleysousukee@gmail.com>
2021-08-15 00:10:26 -0300

    implementa o metodo get

--- resources/hotel.py
+++ resources/hotel.py
@@ -12,18 +12,18 @@ hotels = [
         'estrelas': '4,5',
         'valor': '450,56',
     }, {
-        'id': 'Hotel2',
-        'nome': 'Hotel2',
+        'id': 'Hotel3',
+        'nome': 'Hotel3',
         'estrelas': '4,5',
         'valor': '450,56',
     }, {
-        'id': 'Hotel2',
-        'nome': 'Hotel2',
+        'id': 'Hotel4',
+        'nome': 'Hotel4',
         'estrelas': '4,5',
         'valor': '450,56',
     }, {
-        'id': 'Hotel2',
-        'nome': 'Hotel2',
+        'id': 'Hotel5',
+        'nome': 'Hotel5',
         'estrelas': '4,5',
         'valor': '450,56',
     }
@@ -33,3 +33,20 @@ hotels = [
 class Hotels(Resource):
     def get(self):
         return {'hotels': hotels}
+
+
+class Hotel(Resource):
+    def get(self, id):
+        for hotel in hotels:
+            if hotel['id'] == id:
+                return hotel
+        return {'message': 'Hotel not found!'}, 404
+
+    def post(self):
+        pass
+
+    def put(self):
+        pass
+
+    def delete(self):
+        pass
