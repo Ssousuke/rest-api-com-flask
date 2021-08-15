1f7e4fcaa8da618395b52d6f715123bdd4e16057
ssousuke <wesleysousukee@gmail.com>
2021-08-15 16:53:46 -0300

    mapeia as colunas de HotelModel

--- models/hotel_models.py
+++ models/hotel_models.py
@@ -1,4 +1,13 @@
-class HotelModel:
+from sql_alchemy import db
+
+
+class HotelModel(db.Model):
+    __tablename__ = 'hotels'
+    id = db.Column(db.String, primary_key=True)
+    nome = db.Column(db.String(80))
+    estrelas = db.Column(db.Float(precision=1))
+    valor = db.Column(db.Float(precision=2))
+
     def __init__(self, id, nome, estrelas, valor):
         self.id = id
         self.nome = nome
