1e230a55a424d921a328203ed988af19d9115be4
ssousuke <wesleysousukee@gmail.com>
2021-08-15 16:54:25 -0300

    configura SQLAlchemy no app.py

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from flask import Flask
 from flask_restful import Api
 
-from resources.hotel import Hotels, Hotel
+from resources.hotel_resources import Hotels, Hotel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database-api.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+
+
+@app.before_request
+def create_db():
+    db.create_all()
+
 
 api.add_resource(Hotels, '/hotels')
 api.add_resource(Hotel, '/hotels/<string:id>')
 
-
 if __name__ == '__main__':
+    from sql_alchemy import db
+
+    db.init_app(app)
     app.run(debug=True)
