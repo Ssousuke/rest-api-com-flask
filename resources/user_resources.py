95a3cabbfa0a135383ffab30a4c48acbce7b582a
ssousuke <wesleysousukee@gmail.com>
2021-08-16 01:29:22 -0300

    cria UserRegister e o metodo post

--- resources/user_resources.py
+++ resources/user_resources.py
@@ -18,3 +18,18 @@ class User(Resource):
                 return {'message': 'Internal server error, unable to delete data.'}, 500
             return {'message': 'User been deleted!'}
         return {'message': 'User not found!'}, 404
+
+
+class UserRegister(Resource):
+    def post(self):
+        arguments = reqparse.RequestParser()
+        arguments.add_argument('login', type=str, required=True, help='the login field is cannot be left blank.')
+        arguments.add_argument('password', type=str, required=True, help='the password field is cannot be left blank.')
+
+        data = arguments.parse_args()
+
+        if UserModel.find_by_login(data['login']):
+            return {'message': f"The login {data['login']} already exists."}
+        user = UserModel(**data)
+        user.save_user()
+        return {'message': 'User created successfully.'}
